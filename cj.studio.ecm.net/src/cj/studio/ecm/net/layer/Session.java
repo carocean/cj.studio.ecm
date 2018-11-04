19f6e571ad29e495c05e66558103a4121b948c0c
caroceanjofers <carocean.jofers@icloud.com>
2018-11-04 23:15:18 +0800

    ecm-1.0.20181104.2315

--- cj.studio.ecm.net/src/cj/studio/ecm/net/layer/Session.java
+++ cj.studio.ecm.net/src/cj/studio/ecm/net/layer/Session.java
@@ -12,20 +12,19 @@ public class Session implements ISession {
 	private long createTime;
 	private long lastVisitTime;
 	private SessionInfo info;
-	public Session(String sid,SessionInfo info, List<ISessionEvent> events) {
+
+	public Session(String sid, SessionInfo info, List<ISessionEvent> events) {
 		sessionId = sid;
 		attmap = new HashMap<String, Serializable>(4);
-		this.events=events;
-		this.info=info;
+		this.events = events;
+		this.info = info;
 	}
-	
+
 	@Override
 	public SessionInfo info() {
 		// TODO Auto-generated method stub
 		return info;
 	}
-	
-	
 
 	@Override
 	public long createTime() {
@@ -51,38 +50,40 @@ public class Session implements ISession {
 	public Object attribute(String key) {
 		return attmap.get(key);
 	}
+
 	@Override
-	public void removeAttribute(String key){
+	public void removeAttribute(String key) {
 		attmap.remove(key);
-		if(attmap.containsKey(key)) {
-			for(ISessionEvent e:events){
-				e.doEvent("attributeRemoved", this,key);
+		if (attmap.containsKey(key)) {
+			for (ISessionEvent e : events) {
+				e.doEvent("attributeRemoved", this, key);
 			}
 		}
 	}
+
 	@Override
 	public void attribute(String key, Serializable value) {
-		
-		if(attmap.containsKey(key)) {
-			Object old=attmap.get(key);
+
+		if (attmap.containsKey(key)) {
+			Object old = attmap.get(key);
 			attmap.put(key, value);
-			for(ISessionEvent e:events){
-				e.doEvent("attributeReplace", this,key,value,old);
+			if (!old.equals(value)) {
+				for (ISessionEvent e : events) {
+					e.doEvent("attributeReplace", this, key, value, old);
+				}
 			}
-		}else {
+		} else {
 			attmap.put(key, value);
-			for(ISessionEvent e:events){
-				e.doEvent("attributeAdd", this,key,value);
+			for (ISessionEvent e : events) {
+				e.doEvent("attributeAdd", this, key, value);
 			}
 		}
-		
+
 	}
 
 	@Override
 	public String sid() {
 		return sessionId;
 	}
-
-	
 
 }
